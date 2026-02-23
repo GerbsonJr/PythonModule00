edb8a594c3a2be6240986a5376405b294c090e6d
Junior <gerbsonjunior2007@gmail.com>
2026-02-23 14:45:14 +0000

    estrutura corrigida

--- ex4/ft_water_reminder.py
+++ ex4/ft_water_reminder.py
@@ -1,16 +1,4 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_water_reminder.py                               :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: junior <junior@student.42.fr>              +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/02/18 19:40:53 by junior            #+#    #+#              #
-#    Updated: 2026/02/19 12:43:38 by junior           ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-def	ft_water_reminder():
+def ft_water_reminder():
     water = int(input("Days since last watering: "))
     if water > 2:
         print("Water the plants!")
